60f71430a679daf1cd0749dc10eea637bd8f0b0c
jarydc333@gmail.com <jarydc333@gmail.com>
2019-06-11 09:15:45 -0500

    Added sqlalchemy

--- FitnessTracker/FitnessTracker/FitnessTracker/views.py
+++ FitnessTracker/FitnessTracker/FitnessTracker/views.py
@@ -5,6 +5,7 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FitnessTracker import app
+from flask_sqlalchemy import SQLAlchemy
 
 @app.route('/')
 @app.route('/home')
